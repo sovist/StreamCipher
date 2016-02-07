1291876a16bdd08c093294081b94a94a66c062d1
sovist <sovist9@ukr.net>
2016-02-07 22:08:27 +0200

    fix

--- UsbHidDevice/ValidationKey.cs
+++ UsbHidDevice/ValidationKey.cs
@@ -6,7 +6,7 @@ namespace UsbHidDevice
 {
     public class ValidationKey : INotifyPropertyChanged
     {
-        private byte[] _key = new byte[0];
+        private byte[] _key;
         public byte[] Key
         {
             get { return _key; }
@@ -29,11 +29,7 @@ namespace UsbHidDevice
             set
             {
                 _selectedLengthIndex = value;
-                _key = new byte[Lengths[SelectedLengthIndex]];
-                Generate();
-
                 OnPropertyChanged(nameof(SelectedLengthIndex));
-                OnPropertyChanged(nameof(Key));
             }
         }
 
@@ -41,8 +37,8 @@ namespace UsbHidDevice
 
         public ValidationKey()
         {
-            SelectedLengthIndex = Lengths.Length - 1;
-            Key = new byte[]
+            _selectedLengthIndex = Lengths.Length - 1;
+            _key = new byte[]
             {
                 0xE2, 0x9A, 0x4F, 0x32, 0x4E, 0x91, 0x5C, 0x2C, 0xD5, 0x26, 0x59, 0xB0, 0xC7, 0x8B, 0xA5, 0x5E,
                 0x99, 0x02, 0xB1, 0xD6, 0x16, 0xF8, 0xE8, 0x8E, 0x92, 0x12, 0xE4, 0x2B, 0x6E, 0x7A, 0xEB, 0x2D
@@ -54,7 +50,8 @@ namespace UsbHidDevice
         /// </summary>
         public void Generate()
         {
-            Key = GenerateBytesSequense.Get(Lengths[SelectedLengthIndex]);
+            _key = new byte[Lengths[SelectedLengthIndex]];
+            Key = GenerateBytesSequense.Get(_key.Length);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
